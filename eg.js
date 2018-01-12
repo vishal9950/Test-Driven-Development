0b5726563dfcad4c7a780bb25caf6d55205fcfad
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-12 14:37:46 +0530

    Test null elements in array

--- eg.js
+++ eg.js
@@ -28,9 +28,9 @@ function verifyLength(arr1, arr2) {
 
 //check2
 function verifyIfOnlyNumbers(arr) {
-  for (var i = 0; i < arr.length; i++) {
-    if(typeof arr[i] != 'number' || arr[i] === NaN){
-      console.log("Err2: Supply only valid numbers!");
+  for (let i = 0; i < arr.length; i++) {
+    if(typeof arr[i] != 'number' || arr[i] === NaN || arr[i] === null || arr[i] === Infinity){
+      console.log("Err2: Supply only valid numbers(null, undefined, infinity-not allowed)!");
       return false;
     }
   }
@@ -60,8 +60,8 @@ function verifyIfArray(arr) {
 }
 
 function areEqualArrays(first, second) {
-  var flag = true;
-  for (var i = 0; i < first.length; i++) {
+  let flag = true;
+  for (let i = 0; i < first.length; i++) {
     if(first[i] != second[i]){
       flag = false;
       break;
@@ -70,12 +70,14 @@ function areEqualArrays(first, second) {
   return flag;
 }
 
-console.log('Should add ', areEqualArrays(addArrays([1, 2, 3, 4, 5], [6, 7, 8, 9,10]), [7, 9, 11, 13, 15]));
 
+//For add Arrays
+console.log('Should add valid arrays ', areEqualArrays(addArrays([1, 2, 3, 4, 5], [6, 7, 8, 9,10]), [7, 9, 11, 13, 15]));
 console.log('Should not add irregular arrays ', addArrays([1, 2, 3], [4, 5, 6, 7]) === null);
-
 console.log('Should not add invalid numbers or other types ', addArrays(['r', 4], [9, 8]) === null);
-
 console.log('Should not add empty arrays ', addArrays([], [1, 2, 3]) === null);
-
 console.log('Should not add anything other than arrays ', addArrays({}, [1, 2, 3]) === null);
+
+
+//For verifyIfOnlyNumbers
+console.log('Should not add null values ', verifyIfOnlyNumbers([null, 1]) === false);
