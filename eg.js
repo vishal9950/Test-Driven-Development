e0655e1e5e719a52b700248564c922dacedfbac4
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-12 13:27:18 +0530

    Invalid Numbers

--- eg.js
+++ eg.js
@@ -1,6 +1,7 @@
 function addArrays(first, second) {
   var check1 = verifyLength(first, second);
-  if(check1){
+  var check2 = verifyIfOnlyNumbers(first) && verifyIfOnlyNumbers(second);
+  if(check1 && check2){
     var result = [];
     for (var i = 0; i < first.length; i++) {
       result[i] = first[i] + second[i];
@@ -22,6 +23,16 @@ function verifyLength(arr1, arr2) {
   }
 }
 
+function verifyIfOnlyNumbers(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if(typeof arr[i] != 'number' || arr[i] === NaN){
+      console.log("Err2: Supply only valid numbers!");
+      return false;
+    }
+  }
+  return true;
+}
+
 function areEqualArrays(first, second) {
   var flag = true;
   for (var i = 0; i < first.length; i++) {
@@ -36,3 +47,5 @@ function areEqualArrays(first, second) {
 console.log("Should add ", areEqualArrays(addArrays([1, 2, 3, 4, 5], [6, 7, 8, 9,10]), [7, 9, 11, 13, 15]));
 
 console.log("Should not add irregular arrays ", addArrays([1, 2, 3], [4, 5, 6, 7]) === null);
+
+console.log("Should not add invalid numbers or other types ", addArrays(['r', 4], [9, 8]) === null);
